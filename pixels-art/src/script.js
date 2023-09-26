54ce6f1ce237b08db0c0d9ec1019ab82e7c9f67a
Felipe Padovani <padovani1010@gmail.com>
2023-09-26 14:56:14 -0300

    Tentando resolver 4

--- pixels-art/src/script.js
+++ pixels-art/src/script.js
@@ -1,3 +1,13 @@
-window.onload(createPixelMap());
-
-const createPixelMap = () =>{};
+const createPixelMap = () =>{
+    const pixelBoard = document.querySelector('#pixel-board');
+    const breakRoll = document.createElement('br');
+    for(let i=0;i<5;i+=1){
+        pixelBoard.appendChild(breakRoll);
+        for(let j=0;j<5;j+=1){
+            const newSquare = document.createElement('div');
+            newSquare.classList.add('pixel');
+            pixelBoard.appendChild(newSquare);
+        }
+    }
+};
+window.onload = createPixelMap();
