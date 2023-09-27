3e53f2367431efb19d7f76bedb12ce0a312deed3
Felipe Padovani <padovani1010@gmail.com>
2023-09-27 14:50:27 -0300

    Tentando arrumar o botão limpar

--- pixels-art/script.js
+++ pixels-art/script.js
@@ -1,16 +1,53 @@
+const pixelBoard = document.querySelector('#pixel-board');
+const colorList = document.querySelectorAll('.color');
+const button = document.querySelector('#clear-board');
+const boardButton = document.querySelector('#generate-board');
+const inputValue = document.querySelector('#board-size');
+
+// Pega a quantidade de quadrados atualizada
+const getUpdatedSquaresNumber = () => {
+  return document.querySelectorAll('.pixel').length;
+};
+// Pega a lista atualizadad de elementos
+const getUpdatedSquaresElements = () => {
+  return document.querySelectorAll('.pixel');
+};
+
+// Limpa o quadro de pixels
+function clearBoard() {
+  while (pixelBoard.firstChild) {
+    pixelBoard.removeChild(pixelBoard.firstChild);
+  }
+}
+// Adiciona eventos aos quadrados
+function addEventToSquares() {
+  for (let i = 0; i < getUpdatedSquaresNumber(); i += 1) {
+    const squares = getUpdatedSquaresElements();
+    squares[i].addEventListener('click', () => {
+      for (let j = 0; j < colorList.length; j += 1) {
+        if (colorList[j].classList.contains('selected')) {
+          squares[i].id = colorList[j].id;
+        }
+      }
+    });
+  }
+}
+// Cria o quadro de pixels customizadamente já adicionando evento aos quadrados
 function createPixelBoard(size) {
-  const pixelBoard = document.querySelector('#pixel-board');
+  if (pixelBoard.children.length) clearBoard();
   for (let i = 0; i < size; i += 1) {
     const newSquare = document.createElement('div');
     newSquare.classList.add('pixel');
     pixelBoard.appendChild(newSquare);
   }
+  addEventToSquares();
 }
 createPixelBoard(25);
 
+// Adiciona a cor preta como cor principal
 document.querySelectorAll('.color')[0].classList.add('selected');
 
-const colorList = document.querySelectorAll('.color');
+// Remove e adiciona a class selected na cor selecionada
 for (let i = 0; i < colorList.length; i += 1) {
   colorList[i].addEventListener('click', (event) => {
     for (let j = 0; j <= colorList.length - 1; j += 1) {
@@ -22,27 +59,16 @@ for (let i = 0; i < colorList.length; i += 1) {
   });
 }
 
-const squares = document.querySelectorAll('.pixel');
-for (let i = 0; i < squares.length; i += 1) {
-  squares[i].addEventListener('click', () => {
-    for (let j = 0; j < colorList.length; j += 1) {
-      if (colorList[j].classList.contains('selected')) {
-        squares[i].id = colorList[j].id;
-      }
-    }
-  });
-}
-
-const button = document.querySelector('#clear-board');
+// Função que limpa o quadro
 button.addEventListener('click', () => {
-  for (let i = 0; i < squares.length; i += 1) {
+  const squares = getUpdatedSquaresElements();
+  for (let i = 0; i < getUpdatedSquaresNumber; i += 1) {
+    if (squares[i].id) squares[i].id = '';
     squares[i].style.background = '#FFF';
   }
 });
 
-const boardButton = document.querySelector('#generate-board');
-const inputValue = document.querySelector('#board-size');
-
+// Função que invoca a criação de um novo quadro atravês dos dados do input
 boardButton.addEventListener('click', () => {
   if (!inputValue.value) {
     alert('Board inválido!');
