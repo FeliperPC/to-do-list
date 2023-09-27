f5cafd5e7c208ee36b85d02fceadc3b6dfd488fd
Felipe Padovani <padovani1010@gmail.com>
2023-09-26 21:23:34 -0300

    npm dependencias instaladas

--- pixels-art/src/script.js
+++ pixels-art/src/script.js
@@ -1,13 +1,25 @@
+const pixelBoard = document.querySelector('#pixel-board');
+const breakRoll = document.createElement('br');
+
 const createPixelMap = () =>{
-    const pixelBoard = document.querySelector('#pixel-board');
-    const breakRoll = document.createElement('br');
-    for(let i=0;i<5;i+=1){
-        pixelBoard.appendChild(breakRoll);
-        for(let j=0;j<5;j+=1){
-            const newSquare = document.createElement('div');
-            newSquare.classList.add('pixel');
-            pixelBoard.appendChild(newSquare);
-        }
+    for(let i=0;i<25;i+=1){
+        const newSquare = document.createElement('div');
+        newSquare.classList.add('pixel');
+        pixelBoard.appendChild(newSquare);
     }
 };
-window.onload = createPixelMap();
+
+const colorList = document.querySelectorAll('.color');
+for(let i=0;i<colorList.length;i+=1){
+    colorList[i].addEventListener('click',(event)=>{
+        for(let j=0;j<=colorList.length-1;j+=1){
+            console.log(colorList[j]);
+            if(colorList[j].classList.contains('selected')){
+                colorList[j].classList.remove('selected');
+            }
+        }
+        event.target.classList.add('selected')
+    });
+}
+
+window.onload = createPixelMap(), document.querySelectorAll('.color')[0].classList.add('selected');
