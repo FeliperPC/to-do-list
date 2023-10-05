d900ab0339bfae9fb2e99ee22f7f8fba3ed234a3
Felipe Padovani <padovani1010@gmail.com>
2023-10-04 22:50:01 -0300

    Exercicio 13 e 12 restantes

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,6 +1,11 @@
 const currentTask = document.querySelector('#texto-tarefa');
 const addTaskButton = document.querySelector('#criar-tarefa');
 const olList = document.querySelector('#lista-tarefas');
+const btnRemoveTasks = document.querySelector('#apaga-tudo');
+const btnRemoveCompleted = document.querySelector('#remover-finalizados');
+const btnRemoveSelected = document.querySelector('#remover-selecionado');
+const btnMoveUp = document.querySelector('#mover-cima');
+const btnMoveDown = document.querySelector('#mover-baixo');
 
 // Retorna o texto do input
 const getTaskText = () => document.querySelector('#texto-tarefa').value;
@@ -24,6 +29,16 @@ const removeClassSelected = () => {
   }
 };
 
+// Adciona a classe completed nos elementos que foram clicados 2x e remove se já estiver com a classe completed
+const isCompleted = (event) => {
+  const task = event.target;
+  if (task.classList.contains('completed')) {
+    task.classList.remove('completed');
+  } else {
+    task.classList.add('completed');
+  }
+};
+
 // Muda o background color do elemento selecionado, se possuir a classe selected.
 const changeBackgroundColor = (event) => {
   const elemento = event;
@@ -32,23 +47,10 @@ const changeBackgroundColor = (event) => {
   elemento.target.style.background = 'gray';
 };
 
-// Adciona a classe completed nos elementos que foram clicados 2x
-const isCompleted = (event) => {
-  const elementEvent = event.target;
-  if (!elementEvent.getAttribute('secondClick')) {
-    elementEvent.classList.add('completed');
-    elementEvent.setAttribute('secondClick', false);
-  } else {
-    elementEvent.classList.remove('completed');
-    elementEvent.setAttribute('secondClick', false);
-  }
-  elementEvent.setAttribute('secondClick', true);
-};
-
 // Adiciona Listener aos elementos criados
 const addListenerTask = (task) => {
   task.addEventListener('click', changeBackgroundColor);
-  task.addEventListener('click', isCompleted);
+  task.addEventListener('dblclick', isCompleted);
 };
 
 // Adiciona a tarefa criada na lista
@@ -56,11 +58,75 @@ const addTaskToList = () => {
   const taskItem = document.createElement('li');
   taskItem.innerText = getTaskText();
   taskItem.classList.add('task');
-  taskItem.setAttribute('secondClick', false);
   addListenerTask(taskItem);
   olList.appendChild(taskItem);
   clearInputText();
 };
 
+// Remove todas as tarefas
+const removeAllTasks = () => {
+  while (olList.firstChild) {
+    olList.firstChild.remove();
+  }
+};
+
+// Remove todas as tarefas completadas
+const removeAllCompleted = () => {
+  const list = getListElements();
+  console.log(list);
+  for (let i = 0; i < list.length; i += 1) {
+    if (list[i].classList.contains('completed')) list[i].remove();
+  }
+};
+
+// Remove o elemento selecionado
+const removeSelected = () => {
+  const list = getListElements();
+  for (let i = 0; i < list.length; i += 1) {
+    if (list[i].classList.contains('selected')) list[i].remove();
+  }
+};
+
+// Checa se existe task com seleção
+const checkSelection = () => {
+  const list = getListElements();
+  for (let i = 0; i < list.length; i += 1) {
+    if (list[i].classList.contains('selected')) {
+      return true;
+    }
+  }
+  alert('Selecione uma tarefa primeiro !');
+  return false;
+};
+
+// Move a task para cima
+const moveTaskUp = () => {
+  if (checkSelection()) {
+    const list = getListElements();
+    const array = Array.from(list);
+    console.log(array);
+    for (let i = 0; i < array.length; i += 1) {
+      if (array[i].classList.contains('selected')) {
+        array.splice(i, 1);
+        array.splice(i - 1, 0, array[i]);
+      }
+    }
+  }
+};
+
+// // Move a task para baixo
+// const moveTaskDown = () => {
+//   if (checkSelection()) {
+
+//   } else {
+//     alert('Selecione uma tarefa primeiro !');
+//   }
+// };
+
+// btnMoveDown.addEventListener('click', moveTaskDown);
+btnMoveUp.addEventListener('click', moveTaskUp);
+btnRemoveSelected.addEventListener('click', removeSelected);
+btnRemoveCompleted.addEventListener('click', removeAllCompleted);
+btnRemoveTasks.addEventListener('click', removeAllTasks);
 addTaskButton.addEventListener('click', addTaskToList);
 currentTask.addEventListener('change', getTaskText);
